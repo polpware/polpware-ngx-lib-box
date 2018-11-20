353732be12a23b5b65eaa4097cc3a14849d298dd
Xiaolong <xiaolong@cse.tamu.edu>
2018-11-20 11:43:15 -0500

    Improved url validators

--- projects/polpware-ngx-form/src/lib/validators/url-validator.spec.ts
+++ projects/polpware-ngx-form/src/lib/validators/url-validator.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { buildUrlValidator } from './url-validator';
+import * as Url from 'url-parse';
+
+import { buildUrlValidator, normalizeUrl, normalizeHost } from './url-validator';
 import { FormControl } from '@angular/forms';
 
 describe('urlValidator', () => {
@@ -11,4 +13,62 @@ describe('urlValidator', () => {
 
         expect(validator(ctrl)).toBeNull();
     });
+
+    it('An invalid url', () => {
+        const validator = buildUrlValidator();
+        const ctrl = new FormControl('xpeerofferscom');
+
+        const v = validator(ctrl);
+
+        expect(v).toEqual({ invalid: true });
+    });
+
+    it('Normalize into lowercase', () => {
+        const v = normalizeUrl('http://PeerOffers.com');
+
+        expect(v).toBe('http://peeroffers.com');
+
+    });
+
+    it('Normalize does not remove last forward slash', () => {
+        const v = normalizeUrl('http://PeerOffers.com/');
+
+        expect(v).toBe('http://peeroffers.com/');
+
+    });
+
+    it('host name', () => {
+        const v = Url('http://PeerOffers.com/');
+
+        expect(v.host).toBe('peeroffers.com');
+
+    });
+
+    it('www does matter for host name', () => {
+        const v = Url('http://WWW.PeerOffers.com/');
+
+        expect(v.host).toBe('www.peeroffers.com');
+
+    });
+
+    it('normalizeHost', () => {
+        const v = normalizeHost('http://peeroffers.com');
+        expect(v).toBe('peeroffers.com');
+    });
+
+    it('normalizeHost remove www', () => {
+        const v = normalizeHost('http://www.peeroffers.com');
+        expect(v).toBe('peeroffers.com');
+    });
+
+    it('normalizeHost is case-insensitive', () => {
+        const v = normalizeHost('http://WWW.PEEROFFERS.com');
+        expect(v).toBe('peeroffers.com');
+    });
+
+    it('normalizeHost includes port', () => {
+        const v = normalizeHost('http://WWW.PEEROFFERS.com:65');
+        expect(v).toBe('peeroffers.com:65');
+    });
+
 });
--- projects/polpware-ngx-form/src/lib/validators/url-validator.ts
+++ projects/polpware-ngx-form/src/lib/validators/url-validator.ts
@@ -1,8 +1,8 @@
 import { FormControl } from '@angular/forms';
 
-declare var Url;
+import * as Url from 'url-parse';
 
-interface IUrlParseResult {
+export interface IUrlParseResult {
     protocol: 'http' | 'https';
     slashes: boolean;
     auth: string;
@@ -16,30 +16,79 @@ interface IUrlParseResult {
     hash: string; // # part
     href: string;
     origin: string;
+
+    set(key: string, value: string);
+    toString(): string;
+}
+
+export interface IInvalidSpec {
+    invalid: boolean;
+}
+
+export function validateUrl(value: string, parseQuery: boolean): IInvalidSpec | IUrlParseResult {
+
+    const pattern = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+    const expr = new RegExp(pattern);
+    if (!expr.test(value)) {
+        return { invalid: true };
+    }
+
+    const results = new Url(value, parseQuery) as IUrlParseResult;
+
+    if (!results.protocol || !results.host) {
+        return { invalid: true };
+    }
+
+    return results;
+}
+
+
+/**
+* Given a valid url, returns the normalized host name. 
+* The returned host name does not include www and is lowercase.
+* The returned host includes host number. 
+* @param {} string
+*/
+export function normalizeHost(s: string) {
+    // Assume that s is a valid url
+    const results = new Url(s) as IUrlParseResult;
+    const h = results.host;
+    if (h.startsWith('www')) {
+        return h.substring(4);
+    }
+    return h;
+}
+
+export function normalizeUrl(s: string) {
+    // Assume that s is a valid url
+    const results = new Url(s) as IUrlParseResult;
+    return results.toString();
+}
+
+// Type predicate 
+function isInvalidSpec(v: IUrlParseResult | IInvalidSpec): v is IInvalidSpec {
+    return (<IInvalidSpec>v).invalid !== undefined;
 }
 
 export function buildUrlValidator(options?: {
     https?: boolean;
+    parseQuery?: boolean;
 }) {
 
     const inputs = Object.assign({}, options);
 
     return function(control: FormControl) {
         const value = control.value;
-        const results = new Url(value) as IUrlParseResult;
 
-        if (!results.protocol || !results.host) {
-            return {
-                invalid: true
-            };
+        const results = validateUrl(value, !!inputs.parseQuery);
+
+        if (isInvalidSpec(results)) {
+            return results;
         }
 
         if (inputs.https && results.protocol !== 'https') {
-            return {
-                invalid: true
-            };
+            return { invalid: true };
         }
-
 
         return null;
     };
